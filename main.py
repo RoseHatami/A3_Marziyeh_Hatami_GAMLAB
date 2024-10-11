427c3684d54f16454b921ed3a0353c412115b526
RoseHatami <rose.htmi62@gmail.com>
2024-10-11 23:16:05 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-
-#----------Import Libs------------------
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
-from 
+data = load_breast_cancer()
+x_RealData = data.data
+y_RealData = data.target
 
 
-#----------Import Data------------------
+x_LRTest,x_LRTrain,y_LRTest,y_LRTrain=train_test_split(x_RealData,y,test_size=0.25)
 
+Log_model = LogisticRegression(max_iter=10000)
 
+Log_model.fit(x_LRTrain, y_LRTrain)
 
+y_LRPred=Log_model.predict(x_LRTest)
 
+plt.scatter(y_LRTest, y_LRPred)
+plt.xlabel('True Values')
+plt.ylabel('Predictions')
+plt.show()
 
-
-#-----------Step1 : X and Y ----------------------
+accuracy = Log_model.score(x_RealData, y_RealData)
+print(f"Accuracy: {accuracy}")
